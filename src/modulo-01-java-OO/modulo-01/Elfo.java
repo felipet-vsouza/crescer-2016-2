d84fa5a039c1b45a3f4e68e2ce324d035b8aa9ea
Felipe Souza <felipe.souza@cwi.com.br>
2016-09-27 17:22:57 -0300

    feat(Elfo): implementar exercícios da Lista 1

--- src/modulo-01-java-OO/modulo-01/Elfo.java
+++ src/modulo-01-java-OO/modulo-01/Elfo.java
@@ -2,31 +2,31 @@ public class Elfo {
     private String nome;
     private Item arco;
     private Item flecha;
-    
+    private int exp;
+
     public Elfo(String n) {
         nome = n;
         arco = new Item("Arco", 1);
-        flecha = new Item("Flechas", 2);
+        flecha = new Item("Flechas", 42);
     }
-    
+
     public void setNome(String n) {
         nome = n;
     }
-    
+
     public String getNome() {
         return nome;
     }
-    
+
     public void atirarFlecha() {
         flecha.setQuantidade(flecha.getQuantidade() - 1);
+        exp++;
     }
+
+    /*public void atirarFlechaRefactory(){
+        flecha.setQuantidade(flecha.getQuantidade()-1);
+        experiencia++;
+    }*/
 }
-
-
-
-
-
-
-
 
 
