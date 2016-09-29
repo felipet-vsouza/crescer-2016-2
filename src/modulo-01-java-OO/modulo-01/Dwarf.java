81733317368b383ebea0646e323fbbaeb28afbeb
Felipe Souza <felipe.souza@cwi.com.br>
2016-09-29 01:05:14 -0300

    feat(Dwarf): implementar método getNumeroSorte em Dwarf.

--- src/modulo-01-java-OO/modulo-01/Dwarf.java
+++ src/modulo-01-java-OO/modulo-01/Dwarf.java
@@ -17,6 +17,17 @@ public class Dwarf {
         this.hp -= dano;
     }
     
+    public double getNumeroSorte() {
+        double numero = 101.0;
+        if(this.dataNascimento.ehBissexto() && this.hp >= 80 && this.hp <= 90)
+            numero *= -33;
+        if(!this.dataNascimento.ehBissexto() && (this.nome.equals("Seixas") || this.nome.equals("Meireles"))){
+            numero *= 33;
+            numero = numero % 100;
+        }
+        return numero;
+    }
+    
     public int getHp(){
         return this.hp;
     }
--- src/modulo-01-java-OO/modulo-01/DwarfTest.java
+++ src/modulo-01-java-OO/modulo-01/DwarfTest.java
@@ -60,4 +60,31 @@ public class DwarfTest
         assertEquals(12, pedrao.getDataNascimento().getMes());
         assertEquals(1004, pedrao.getDataNascimento().getAno());
     }
+    
+    @Test
+    public void dwarfPossuiNumeroDaSorte101() {
+        // Arrange
+        Dwarf youtuber = new Dwarf("Pedro Pedroso", new DataTerceiraEra(31, 1, 2713));
+        // Act & Assert
+        assertEquals(101.0, youtuber.getNumeroSorte(), 0.01);
+    }
+    
+    @Test
+    public void dwarfPossuiNumeroDaSorteMenos3333() {
+        // Arrange
+        Dwarf jamelao = new Dwarf("Jamelão", new DataTerceiraEra(2, 2, 2000));
+        jamelao.recebeDano(10);
+        jamelao.recebeDano(10);
+        jamelao.recebeDano(10);
+        // Act & Assert
+        assertEquals(-3333.0, jamelao.getNumeroSorte(), 0.01);
+    }
+    
+    @Test
+    public void dwarfPossuiNumeroDaSorte33() {
+        // Arrange
+        Dwarf joker = new Dwarf("Seixas", new DataTerceiraEra(14, 8, 1967));
+        // Act & Assert
+        assertEquals(33.0, joker.getNumeroSorte(), 0.01);
+    }
 }
