cdb4345fadab16464c92097783d1207a3ab95a25
Felipe Souza <felipe.souza@cwi.com.br>
2016-10-25 00:41:35 -0200

    feat(exercicio1): concluir exercícios do dia 24/10/2016.

--- Módulo 4 - Javascript/24-10-2016/exercicio1.js
+++ Módulo 4 - Javascript/24-10-2016/exercicio1.js
@@ -66,3 +66,158 @@ console.log(find([{ nome: 'a' }, { nome: 'b' }], function (elem) {
 
 console.log(find([1, 2, 3], 1));
 // undefined
+
+
+/* 4. Você sabe subtrair?
+Crie uma função chamada subtrair que realiza a subtração de dois números em duas chamadas de funções diferentes. NÃO SURTEM! */
+var subtrair = function (num1) {
+    return function(num2) {
+        return num1 - num2;
+    }
+}
+console.log(subtrair(2)(1)); // 1
+console.log(subtrair(0)(0)); // 0
+console.log(subtrair(-1)(2)); // -3
+
+/* 5. Igualdade recursiva
+Escreva uma função iguais que recebe dois parâmetros e retorna true apenas se os valores dos parâmetros forem 
+iguais ou se eles forem objetos com as mesmas propriedades com valores iguais e assim recursivamente 
+(ou seja, em até vários níveis de propriedades dentro dos objetos). */
+var iguais = function(obj1, obj2) {
+    if(obj1 === null || typeof obj1 !== "object") {
+        if(obj1 === obj2)
+            return true;
+        else
+            return false;
+    } else {
+        for(var prop in obj1) {
+            if(!obj1.hasOwnProperty(prop) || !obj2.hasOwnProperty(prop) || !iguais(obj1[prop], obj2[prop]))
+                return false;
+        }
+        return true;
+    } 
+}
+
+// TESTES
+var obj = { a: { a: 'a' }, b: 2 };
+console.log(iguais(obj, obj)); // true
+console.log(iguais(obj, { a: 1, b: 2 })); // false
+console.log(iguais(obj, { a: { a: 'a' }, b: 2 })); // true
+console.log(iguais({ a: 1 }, { b: 1 })); // false
+var goku = {
+  nivel: 'SSJ4',
+  golpes: [ { nome: 'kamehameha', dano: 45 }, { nome: 'genki-dama', dano: 99 } ],
+  nome: 'Goku',
+}, sonGoku = {
+  nome: 'Goku',
+  nivel: 'SSJ4',
+  golpes: [ { nome: 'kamehameha', dano: 45 }, { nome: 'genki-dama', dano: 99 } ]
+};
+console.log(iguais(goku, sonGoku)); // true
+console.log(iguais({ a: 1 }, { a: 1, b: 2 })); // false
+goku = {
+  nivel: 'SSJ4',
+  golpes: [ { nome: 'kamehameha', dano: { precisao: 'milhar', valor: 56 } }, { nome: 'genki-dama', dano: 99 } ],
+  nome: 'Goku',
+}, sonGoku = {
+  nivel: 'SSJ4',
+  golpes: [ { nome: 'kamehameha', dano: { precisao: 'milhar', valor: 56 } }, { nome: 'genki-dama', dano: 99 } ],
+  nome: 'Goku',
+};
+console.log(iguais(goku, sonGoku)); // true
+goku = {
+  nome: 'Goku',
+  nivel: 'SSJ4',
+  golpes: [
+    { nome: 'kamehameha', dano: 45 }, { nome: 'genki-dama', dano: 99 }
+  ]
+}, sonGoku = {
+  nome: 'Goku',
+  nivel: 'SSJ4',
+  golpes: [
+    { nome: 'genki-dama', dano: 99 }, { nome: 'kamehameha', dano: 45 }
+  ]
+};
+console.log(iguais(goku, sonGoku)); // false
+
+/* 6. Ctrl+Copyright
+Escreva uma função ctrlC que recebe um parâmetro e realiza a cópia de todos seus valores e retorne o resultado. NÃO SURTEM!!  */
+var ctrlC = function(objeto) {
+    var ret = {};
+    for(var property in objeto) {
+        ret[property] = objeto[property];
+    }
+    return ret;
+}
+
+var origem = { a: '1' };
+var destino = ctrlC(origem);
+console.log(destino);
+// { a: '1' }
+console.log(origem === destino);
+// false
+console.log(iguais(origem, destino));
+// true
+destino = ctrlC({ a: [ { b: '1', c: '2' }, { d: false }, { e: function() { return 3; } } ] });
+console.log(destino.a[2].e());
+// 3
+
+/*  7. Mesclar
+Crie uma função chamada mesclar que recebe dois objetos como argumentos e mescla todas propriedades do segundo parâmetro dentro do primeiro. NÃO SURTEM!!!  */
+var mesclar = function(obj1, obj2, recursiva) {
+    if(recursiva === false) {
+        for(var prop in obj2) {
+            obj1[prop] = obj2[prop];
+        }
+    } else {
+        if(typeof obj1 === 'object') {
+            for(var prop in obj2) {
+                mesclar(obj1[prop], obj2[prop], recursiva);
+            }
+        } else {
+            obj1 = obj2;
+        }
+    } 
+}
+
+//TESTE
+var objeto1 = {
+  abacaxi: 0,
+  banana: { peso: 52, preco: 100 },
+  cereja: 97
+};
+var objeto2 = {
+  banana: { preco: 200 },
+  damasco: 100
+};
+mesclar(objeto1, objeto2);
+console.log(objeto1);
+// { abacaxi: 0, banana: { preco: 200 }, cereja: 97, damasco: 100 }
+
+/* BÔNUS
+Adicione um terceiro parâmetro na função mesclar que, quando for true, faça a operação de mescla ser recursiva */
+var objeto1 = {
+  abacaxi: 0,
+  banana: { peso: 52, preco: 100, origem: { cidade: 'taquari', estado: 'rs' } },
+  cereja: 97
+};
+var objeto2 = {
+  banana: { preco: 200, origem: { cidade: 'cachoeira do sul', estado: 'rs' } },
+  damasco: 100
+};
+mesclar(objeto1, objeto2, true);
+console.log(objeto1);
+/*
+{
+  abacaxi: 0,
+  banana: {
+    peso: 52,
+    preco: 200,
+    origem: {
+      cidade: 'cachoeira do sul', estado: 'rs'
+    }
+  },
+  cereja: 97,
+  damasco: 100
+}
+*/
