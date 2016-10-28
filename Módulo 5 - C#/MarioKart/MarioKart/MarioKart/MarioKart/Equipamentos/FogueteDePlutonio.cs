84c96e1ec8dacab2a209dcd6efecafff3a7cc9e8
Felipe Souza <felipe.souza@cwi.com.br>
2016-10-28 21:20:04 -0200

    feat(Mario Kart): implementar FogueteDePlutonio..

---
+++ Módulo 5 - C#/MarioKart/MarioKart/MarioKart/MarioKart/Equipamentos/FogueteDePlutonio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class FogueteDePlutonio : IEquipamento
+    {
+        public FogueteDePlutonio(int nivel = 2)
+        {
+            if(nivel < 1 || nivel > 5)
+            {
+                nivel = 2;
+            }
+            this.Nivel = nivel;
+        }
+
+        public int Nivel { get; private set; }
+
+        public int Bonus
+        {
+            get
+            {
+                return this.Nivel;
+            }
+        }
+    }
+}
---
+++ Módulo 5 - C#/MarioKart/MarioKart/MarioKart/UnitTestProject1/EquipamentoTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MarioKart;
+using MarioKart.Equipamentos;
+using MarioKart.Karts;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class EquipamentoTest
+    {
+        [TestMethod]
+        public void FogueteABaseDePlutonioNivel2Da2DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new FogueteDePlutonio());
+            Assert.AreEqual(10, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void FogueteABaseDePlutonioNivel5Da5DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new FogueteDePlutonio(5));
+            Assert.AreEqual(13, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void FogueteABaseDePlutonioComNivelInvalidoAssumeNivel2()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new FogueteDePlutonio(Int32.MaxValue));
+            Kart boi = new Light(new Corredor("Peão de Guarulhos", NivelHabilidade.Mediano));
+            boi.Equipar(new FogueteDePlutonio(0));
+            Assert.AreEqual(10, kart.Velocidade);
+            Assert.AreEqual(10, boi.Velocidade);
+        }
+    }
+}
