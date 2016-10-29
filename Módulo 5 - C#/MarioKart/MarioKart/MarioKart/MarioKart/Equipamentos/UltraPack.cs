fe9014124c613014793227a0f8c316ac38627c3f
Felipe Souza <felipe.souza@cwi.com.br>
2016-10-28 22:26:37 -0200

    feat(Mario Kart): implementar UltraPack.

---
+++ Módulo 5 - C#/MarioKart/MarioKart/MarioKart/MarioKart/Equipamentos/UltraPack.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarioKart.Equipamentos
+{
+    public class UltraPack : IEquipamento
+    {
+        public UltraPack(IEquipamento equipamento)
+        {
+            this.Equipamento = equipamento;
+        }
+
+        public IEquipamento Equipamento { get; private set; }
+
+        public int Bonus
+        {
+            get
+            {
+                return (int) Math.Ceiling(1.2 * this.Equipamento.Bonus);
+            }
+        }
+    }
+}
--- Módulo 5 - C#/MarioKart/MarioKart/MarioKart/UnitTestProject1/EquipamentoTest.cs
+++ Módulo 5 - C#/MarioKart/MarioKart/MarioKart/UnitTestProject1/EquipamentoTest.cs
@@ -35,5 +35,74 @@ namespace UnitTestProject1
             Assert.AreEqual(10, kart.Velocidade);
             Assert.AreEqual(10, boi.Velocidade);
         }
+
+        [TestMethod]
+        public void UltraPackComFogueteDePlutonioNivel5Da6DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new FogueteDePlutonio(5)));
+            Assert.AreEqual(14, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void UltraPackComFogueteDePlutonioNivel1Da2DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new FogueteDePlutonio(1)));
+            Assert.AreEqual(10, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void UltraPackComFogueteDePlutonioNivel3Da4DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new FogueteDePlutonio(3)));
+            Assert.AreEqual(12, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void UltraPackComMotorABaseDeLavaDa4DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new MotorABaseDeLava()));
+            Assert.AreEqual(12, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void UltraPackComPneusDeCouroDeDragaoDa3DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new PneusDeCouroDeDragao()));
+            Assert.AreEqual(11, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void UltraPackEm2UltraPacksComFogueteDePlutonioNivel3Da6DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new UltraPack(new UltraPack(new FogueteDePlutonio(3)))));
+            Assert.AreEqual(14, kart.Velocidade);
+        }
+
+        [TestMethod]
+        public void UltraPackEm2UltraPacksComFogueteDePlutonioNivel5Da10DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new UltraPack(new UltraPack(new FogueteDePlutonio(5)))));
+            Assert.AreEqual(18, kart.Velocidade);
+        }
+
+
+        [TestMethod]
+        public void UltraPackEm20UltraPacksComFogueteDePlutonioNivel5Da10DeBonus()
+        {
+            Kart kart = new Light(new Corredor("Lula Molusco", NivelHabilidade.Mediano));
+            kart.Equipar(new UltraPack(new UltraPack(new UltraPack(new UltraPack(new UltraPack
+                (new UltraPack(new UltraPack(new UltraPack(new UltraPack(new UltraPack
+                (new UltraPack(new UltraPack(new UltraPack(new UltraPack(new UltraPack
+                (new UltraPack(new UltraPack(new UltraPack(new UltraPack(new UltraPack
+                (new UltraPack(new FogueteDePlutonio(5)))))))))))))))))))))));
+            Assert.AreEqual(316, kart.Velocidade);
+        }
     }
 }
