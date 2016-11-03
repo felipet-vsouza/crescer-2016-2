cc4e30523b6bb584005eefe91586cf1006303377
Felipe Souza <felipe.souza@cwi.com.br>
2016-11-03 16:49:06 -0200

    feat(exercício-3-sfweb): separar solução em camadas.

---
+++ Módulo 5 - C#/StreetFighter.Web/StreetFighter.Web/StreetFighter.Testes/Aplicativo/PersonagemAplicativoTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StreetFighter.Dominio;
+using StreetFighter.Repositorio;
+using FakeItEasy;
+using StreetFighter.Aplicativo;
+
+namespace StreetFighter.Testes.Aplicativo
+{
+    [TestClass]
+    public class PersonagemAplicativoTest
+    {
+        [TestMethod]
+        public void PersonagemComId0DeveSerIncluido()
+        {
+            var repositorio = A.Fake<IPersonagemRepositorio>();
+            var personagem = new Personagem("Blanka", "Rio Negro e Solimões");
+            var aplicativo = new PersonagemAplicativo(repositorio);
+
+            aplicativo.Salvar(personagem);
+
+            A.CallTo(() => repositorio.IncluirPersonagem(personagem)).MustHaveHappened();
+        }
+    }
+}
