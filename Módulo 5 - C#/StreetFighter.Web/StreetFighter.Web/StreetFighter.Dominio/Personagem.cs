63c67d09a3cb3c5ef8543b34e0840e8060e0a8ac
Felipe Souza <felipe.souza@cwi.com.br>
2016-11-08 15:32:31 -0200

    feat(ef): criar projeto de repositório utilizando Entity Framework.

--- Módulo 5 - C#/StreetFighter.Web/StreetFighter.Web/StreetFighter.Dominio/Personagem.cs
+++ Módulo 5 - C#/StreetFighter.Web/StreetFighter.Web/StreetFighter.Dominio/Personagem.cs
@@ -8,15 +8,15 @@ namespace StreetFighter.Dominio
 {
     public class Personagem
     {
-        public int Id { get; private set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
-        public DateTime Nascimento { get; private set; }
-        public int Altura { get; private set; }
-        public decimal Peso { get; private set; }
-        public string Origem { get; private set; }
-        public string Imagem { get; private set; }
-        public string GolpesEspeciais { get; private set; }
-        public bool PersonagemOculto { get; private set; }
+        public DateTime Nascimento { get; set; }
+        public int Altura { get; set; }
+        public decimal Peso { get; set; }
+        public string Origem { get; set; }
+        public string Imagem { get; set; }
+        public string GolpesEspeciais { get; set; }
+        public bool PersonagemOculto { get; set; }
 
         public Personagem(int id, string nome, DateTime nascimento, int altura, decimal peso, string origem, string imagem,
             string golpesEspeciais, bool personagemOculto)
---
+++ Módulo 5 - C#/StreetFighter.Web/StreetFighter.Web/StreetFighter.RepositorioEF/ContextoDatabase.cs
@@ -0,0 +1,28 @@
+﻿using StreetFighter.Dominio;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFighter.RepositorioEF
+{
+    public class ContextoDatabase : DbContext
+    {
+        public ContextoDatabase() : base("sfwDB")
+        {
+
+        }
+
+        public DbSet<Personagem> Personagem { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
