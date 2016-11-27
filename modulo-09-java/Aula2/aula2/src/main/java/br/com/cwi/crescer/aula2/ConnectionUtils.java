b537607285071e9535bb39b7833fa096a2bce86a
Unknown <felipe.souza@cwi.com.br>
2016-11-27 00:37:39 -0200

    chore(ConnectionUtils.java): reconfigurar conexão com o banco.

--- modulo-09-java/Aula2/aula2/src/main/java/br/com/cwi/crescer/aula2/ConnectionUtils.java
+++ modulo-09-java/Aula2/aula2/src/main/java/br/com/cwi/crescer/aula2/ConnectionUtils.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionUtils {
 
-    private static final String url = "jdbc:oracle:thin:@10.99.2.45:1521:xe";
-    private static final String user = "cwi";
-    private static final String pass = "cwi";
+    private static final String url = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String user = "SYSTEM";
+    private static final String pass = "lipinh71";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, pass);
--- modulo-09-java/Aula2/aula2/src/main/java/br/com/cwi/crescer/aula2/PessoaFactory.java
+++ modulo-09-java/Aula2/aula2/src/main/java/br/com/cwi/crescer/aula2/PessoaFactory.java
@@ -11,6 +11,7 @@ import java.util.List;
 public class PessoaFactory {
 
     public static void main(String[] args) {
+        
     }
 
     public static void insert() {
