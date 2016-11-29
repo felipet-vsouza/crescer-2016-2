e68c585fe451b97d2346d25d798fa39c634af6de
Felipe Souza <felipe.souza@cwi.com.br>
2016-11-29 00:52:35 -0200

    feat(Pessoa.java): definir sequence de geração para coluna Id.

--- modulo-09-java/Aula3/aula3/src/main/java/br/com/cwi/crescer/entity/Pessoa.java
+++ modulo-09-java/Aula3/aula3/src/main/java/br/com/cwi/crescer/entity/Pessoa.java
@@ -4,7 +4,10 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import static javax.persistence.GenerationType.SEQUENCE;
 import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
@@ -12,6 +15,8 @@ import javax.persistence.Table;
 public class Pessoa implements Serializable {
 
     @Id // Identifica a PK
+    @GeneratedValue(strategy = SEQUENCE, generator = "SEQ_PESSOA")
+    @SequenceGenerator(name = "SEQ_PESSOA", sequenceName = "SEQ_PESSOA", allocationSize = 1) 
     @Basic(optional = false)
     @Column(name = "ID_PESSOA")
     private Long idPessoa;
